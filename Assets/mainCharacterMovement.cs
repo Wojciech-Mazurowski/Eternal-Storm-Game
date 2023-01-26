d839f63c6ab77cde1bea93fb77e3e11a46d19224
Wojciech-Mazurowski-digimuth <wojciech.mazurowski@digimuth.com>
2023-01-26 15:09:15 +0100

    things

--- Assets/mainCharacterMovement.cs
+++ Assets/mainCharacterMovement.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class mainCharacterMovement : MonoBehaviour
 {
 
     public float speed;
     public float size;
-    public float hp;
+    public int numberOfHearths;
+    public int health;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart; 
         
     // Start is called before the first frame update
     void Start()
     {
         var localScale = transform.localScale;
         transform.localScale = new Vector3(localScale.x*size,localScale.y*size,localScale.z);
-        hp=3;
+
     }
 
     // Update is called once per frame
@@ -23,11 +28,29 @@ public class mainCharacterMovement : MonoBehaviour
         transform.Translate(
             Input.GetAxis("Horizontal") * speed * Time.deltaTime,
             Input.GetAxis("Vertical") * speed * Time.deltaTime, 0f);
+
+        if(health > numberOfHearths){
+            health = numberOfHearths;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i < health){
+                hearts[i].sprite = fullHeart;
+            }else{
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if(i < numberOfHearths){
+                hearts[i].enabled = true;
+            }else{
+                hearts[i].enabled = false;
+            }
+        }
     }
 
     public void DecrementHp(){
-        hp -=1;
-        Debug.Log(hp);
+        health -=1;
     }
 
     private void OnParticleCollision(GameObject other) {
