4e5e628b5517f25647b4249fc21a5a5f4045c531
Wojciech-Mazurowski-digimuth <wojciech.mazurowski@digimuth.com>
2023-01-28 00:14:39 +0100

    load scorew

--- Assets/ScoresLoader.cs
+++ Assets/ScoresLoader.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using TMPro;
 using UnityEngine;
@@ -14,16 +16,28 @@ public class ScoresLoader : MonoBehaviour
     {
         string destination = Application.persistentDataPath + "/save.txt";
         FileStream file;
-
+        List<long> linesNum= new List<long>();
         if (File.Exists(destination)) file = File.OpenRead(destination);
         else
         {
             Debug.LogError("File not found");
             return;
         }
+        string[] lines = System.IO.File.ReadAllLines(destination);
+        foreach (string line in lines)
+        {
+            linesNum.Add(Int64.Parse(line));
+        }
 
-       
-        //Debug.Log(data);
+        linesNum.OrderByDescending(i => i);
+
+        var mid = Math.Min(textMeshProUGUIs.Count, linesNum.Count);
+        for(int i= 0; i<mid; i++)
+        {
+            Debug.Log(mid);
+            textMeshProUGUIs[i].enabled = true;
+            textMeshProUGUIs[i].text = linesNum[i].ToString();
+        }
     }
 
     // Start is called before the first frame update
